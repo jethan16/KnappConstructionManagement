d9c88eeebdb7aeba4361b5d2a9930f8337307616
Jethan16 <ethan.nel.jones@gmail.com>
2022-03-01 17:02:35 -0500

    updated footer styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,20 +11,23 @@ const Footer = () => {
                 <img width="32px" src="https://www.datocms-assets.com/48980/1646140723-kcm_icon-o.png" />
                 <ContactInfoWrapper>
                     <div>
-                        <h6><strong>Mailing Address:</strong></h6>
-                        <h6>7935 Airport Pulling Road, Ste 4 - Box 352</h6>
-                        <h6>Naples, FL</h6>
-                        <h6>34109</h6>
+                        <h5><strong>Mailing Address:</strong></h5>
+                        <h5>7935 Airport Pulling Road, Ste 4 - Box 352</h5>
+                        <h5>Naples, FL</h5>
+                        <h5>34109</h5>
                     </div>
-                    <hr />
+                </ContactInfoWrapper>
+                <ContactInfoWrapper>
+
                     <div>
-                        <h6><strong>Phone:</strong></h6>
-                        <a href="tel:12396758333"><h6>1-239-675-8333</h6></a>
+                        <h5><strong>Phone:</strong></h5>
+                        <a href="tel:12396758333"><h5>1-239-675-8333</h5></a>
                     </div>
-                    <hr />
+                </ContactInfoWrapper>
+                <ContactInfoWrapper>
                     <div>
-                        <h6><strong>Email:</strong></h6>
-                        <a href="mailto:gknapp@knappcm.com"><h6>Gknapp@knappcm.com</h6></a>
+                        <h5><strong>Email:</strong></h5>
+                        <a href="mailto:gknapp@knappcm.com"><h5>Gknapp@knappcm.com</h5></a>
                     </div>
                 </ContactInfoWrapper>
             </FooterInfo>
@@ -38,16 +41,21 @@ const Footer = () => {
 export default Footer;
 
 const FooterWrapper = styled.div`
+    max-width: 100vw;
+    margin: 0;
     position: relative;
-    padding: 50px 25px;
+    padding: 100px 25px;
     display: flex;
+    flex-wrap: wrap;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
 `
 const FooterInfo = styled.div`
     color: white;
     margin: 10px 0;
-    h6 {
+    display: flex;
+    flex-direction: column;
+    h5 {
         margin: 0;
     }
     img {
@@ -55,16 +63,24 @@ const FooterInfo = styled.div`
         top: 25px; 
         left: 25px;
     }
+    @media (min-width: 768px) {
+        flex-direction: row;
+    }
 `
 const ContactInfoWrapper = styled.div`
     display: flex;
-    margin: 10px;
-    div {
-        margin: 0 15px;
-        padding: 0 10px;
-    }
+    margin: 10px 0;
     img {
         width: 32px;
         filter: brightness(0) invert(1);
+        position: absolute;
+        top: 25px;
+        right: 25px;
+    }
+    @media (min-width: 768px) {
+        div {
+            margin: 0 15px;
+            padding: 0 10px;
+        }
     }
 `
